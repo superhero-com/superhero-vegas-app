e92debb35a5c768582d12458f32a1c0925d57b92
sunbaixin <283122529@qq.com>
2021-07-14 13:58:11 +0800

    update nva

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,40 +2,24 @@
     <div class="sidebar">
 
         <div class="logo">
-            <box-icon name='twitter' type='logo' size='40px'></box-icon>
+            <img width="272" src="../assets/logo_vegas.png" alt="">
         </div>
         <ul class="nav-list">
-            <li>
-                <a class='item' href="#">
-                    <box-icon class="icon" type='solid' name='home-smile'></box-icon>
-                    <span class="text">Home</span>
+            <li v-for="(item,index) in nva_list" :key="index">
+                <a :class='item.nva_class' href="#" @mouseover="mouseover(item.text)" @mouseleave="mouseleave">
+                    <box-icon class="icon" :type='item.icon_type' :color="item.icon_color" :name='item.icon'></box-icon>
+                    <span class="text">{{ item.text }} </span>
                 </a>
             </li>
-            <li>
-                <a class='item' href="#">
-                    <box-icon class="icon" name='bitcoin' type='logo'></box-icon>
-                    <span class="text">My Bet</span>
-                </a>
-            </li>
-
-
         </ul>
         <span class="group">Financial Bet</span>
         <ul class="nav-list">
-            <li>
-                <a class='item' href="#">
-                    <box-icon class="icon" type='solid' name='home-smile'></box-icon>
-                    <span class="text">Home</span>
+            <li v-for="(item,index) in financial_list" :key="index">
+                <a :class='item.nva_class' href="#" @mouseover="mouseover(item.text)" @mouseleave="mouseleave">
+                    <box-icon class="icon" :type='item.icon_type' :color="item.icon_color" :name='item.icon'></box-icon>
+                    <span class="text">{{ item.text }} </span>
                 </a>
             </li>
-            <li>
-                <a class='item' href="#">
-                    <box-icon class="icon" name='bitcoin' type='logo'></box-icon>
-                    <span class="text">My Bet</span>
-                </a>
-            </li>
-
-
         </ul>
     </div>
 
@@ -44,15 +28,90 @@
 
 <script>
 
-
     export default {
         comments: {},
         name: 'Navigation',
         props: {
             msg: String
         },
+        data() {
+            return {
+                nva_list: [
+                    {
+                        text: 'Home',
+                        nva_class: 'item',
+                        icon: 'home-smile',
+                        icon_type: 'solid',
+                        icon_color: '#000000'
+                    },
+                    {
+                        text: 'My Bet',
+                        nva_class: 'item',
+                        icon: 'user-circle',
+                        icon_type: 'solid',
+                        icon_color: '#000000'
+                    },
+                    {
+                        text: 'Favorites',
+                        nva_class: 'item',
+                        icon: 'bookmarks',
+                        icon_type: 'solid',
+                        icon_color: '#000000'
+                    }
+                ],
+
+                financial_list: [
+                    {
+                        text: 'Coin',
+                        nva_class: 'item',
+                        icon: 'bitcoin',
+                        icon_type: 'logo',
+                        icon_color: '#000000'
+                    },
+                    {
+                        text: 'Stock',
+                        nva_class: 'item',
+                        icon: 'line-chart',
+                        icon_color: '#000000'
+                    }
+                ],
+
+                sport_list: [
+                    {
+                        text: 'Coin',
+                        nva_class: 'item',
+                        icon: 'football',
+                        icon_color: '#000000'
+                    },
+                    {
+                        text: 'basketball',
+                        nva_class: 'item',
+                        icon: 'basketball',
+                        icon_color: '#000000'
+                    }
+                ],
+            }
+        },
+        methods: {
+            mouseover : function (index) {
+
+                for (let i = 0; i < this.nva_list.length; i++) {
+                    this.nva_list[i].icon_color = '#000000';
+                    this.nva_list[i].nva_class = 'item';
+                }
+                this.nva_list[index].icon_color = '#ff1000';
+                this.nva_list[index].nva_class = 'item-hover';
+            },
+            mouseleave: function () {
+
+                for (let i = 0; i < this.nva_list.length; i++) {
+                    this.nva_list[i].icon_color = '#000000';
+                    this.nva_list[i].nva_class = 'item';
+                }
+            }
 
 
+        }
     }
 
 </script>
@@ -64,24 +123,26 @@
         box-sizing: border-box;
         font-family: "Poppins", sans-serif;
     }
-    .sidebar{
+
+    .sidebar {
         margin-bottom: 10px;
     }
 
     .logo {
-        background-color: antiquewhite;
         height: 100px;
         display: flex;
         align-items: center;
         justify-content: center;
     }
-    .group{
+
+    .group {
         text-align: left;
         display: flex;
         align-items: center;
         padding-left: 20px;
         margin-left: 20px;
         margin-top: 30px;
+        margin-bottom: 10px;
     }
 
     .item {
@@ -95,11 +156,22 @@
     }
 
 
-    .item:hover {
+    .item-hover {
+
+        height: 50px;
+        display: flex;
+        align-items: center;
+        margin-left: 20px;
+        margin-right: 20px;
+        margin-top: 10px;
+        padding-left: 20px;
+
         border-radius: 10px;
         background-color: aquamarine;
-        /*color: #fa169f;*/
-
+        color: #fa169f;
+        .text {
+            color: #fa169f;
+        }
 
     }
 
